80cf8b044f9ec793f3aae68a4441f9fb5aa6fc15
oguni <oguni@b-forme.net>
2020-08-26 16:07:34 +0900

    【API】Blog API用のマイグレーション作成

---
+++ database/migrations/2020_08_26_155408_create_blogs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('tag')->comment('タグ');
+            $table->string('title')->comment('タイトル');
+            $table->string('content')->comment('内容');
+            $table->date('date')->comment('日付');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blogs');
+    }
+}
